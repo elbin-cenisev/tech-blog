a62d4b39774efeff3213f244772c212acbf26b9d
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-31 19:20:30 -0400

    Created register route to create new users

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -9,3 +9,20 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 })
+
+router.post('/register', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+
+      res.status(200).json(userData);
+    });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+module.exports = router;
