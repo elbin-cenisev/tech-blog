7966660dbb205ff6db17248c6bd5278b6509cca1
Elbin Cenisev <elbincenisev@outlook.com>
2021-08-31 18:18:19 -0400

    Added basic script to handle link buttons

---
+++ public/js/links.js
@@ -0,0 +1,19 @@
+document.getElementById("homeBtn").addEventListener("click", function()
+{ 
+  alert("Hello Home!"); 
+});
+
+document.getElementById("dashboardBtn").addEventListener("click", function()
+{ 
+  alert("Hello Dashboard!"); 
+});
+
+document.getElementById("loginBtn").addEventListener("click", function()
+{ 
+  alert("Hello Login!"); 
+});
+
+document.getElementById("logoutBtn").addEventListener("click", function()
+{ 
+  alert("Hello Logout!"); 
+});
