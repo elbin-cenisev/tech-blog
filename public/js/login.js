f34ade61a3a9862d0ddc5aafb8a3085bf8690043
Elbin Cenisev <elbincenisev@outlook.com>
2021-09-01 17:17:43 -0400

    added login form handler

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,4 @@
+// Handles new user registration
 document.getElementById("registerBtn").addEventListener("click", async function () {
     event.preventDefault();
 
@@ -17,5 +18,27 @@ document.getElementById("registerBtn").addEventListener("click", async function 
         } else {
             alert(response.statusText);
         }
+    }
+});
+
+// Handles user login
+document.getElementById("loginBtn").addEventListener("click", async function () {
+    event.preventDefault();
+
+    const email = document.querySelector('#loginEmail').value.trim();
+    const password = document.querySelector('#loginPassword').value.trim();
+  
+    if (email && password) {
+      const response = await fetch('/api/users/login', {
+        method: 'POST',
+        body: JSON.stringify({ email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/');
+      } else {
+        alert(response.statusText);
+      }
     }
 });
