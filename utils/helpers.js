bb7ca7e3d466edafadc8896d2b072a7824c46dca
Elbin Cenisev <elbincenisev@outlook.com>
2021-09-03 07:25:50 -0400

    Date is now formatted correctly

--- utils/helpers.js
+++ utils/helpers.js
@@ -2,6 +2,6 @@ module.exports = {
   // the helper method 'format_time' will take in a timestamp and return a string with only the time
   format_time: (date) => {
     // We use the 'toLocaleTimeString()' method to format the time as H:MM:SS AM/PM
-    return date.toLocaleTimeString();
+    return ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
   },
 };
