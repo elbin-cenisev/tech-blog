e791b91a82dd86b61f04a56a44440da80f0a79b8
Elbin Cenisev <elbincenisev@outlook.com>
2021-09-04 20:09:26 -0400

    Clicking on post now redirects to post

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -14,7 +14,15 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', withAuth, async (req, res) => {
     try {
-        const postData = await Post.findByPk(req.params.id);
+        const postData = await Post.findByPk(req.params.id, {
+            include: [
+                {
+                    model: User,
+                    attributes: ['name'],
+                },
+            ],
+        });
+
         const post = postData.get({ plain: true });
         res.render('post', { post });
     } catch (err) {
